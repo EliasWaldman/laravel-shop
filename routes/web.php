2b2f4c9e936bab02521861246950389c489f2977
EliasWaldman <awers2003@gmail.com>
2024-09-07 09:46:47 +0400

    add files

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ use Laravel\Socialite\Facades\Socialite;
 Route::controller(AuthController::class)->group(function (){
     //login
     Route::get('/login', 'index')->name('login');
-    Route::post('/login', 'signIn')->name('signIn');
+    Route::post('/login', 'signIn')->middleware('throttle:auth')->name('signIn');
     //signUp (register)
     Route::get('/sign-up', 'signUp')->name('signUp');
-    Route::post('/sign-up', 'store')->name('store');
+    Route::post('/sign-up', 'store')->middleware('throttle:auth')->name('store');
     //logout
     Route::delete('/logout', 'logOut')->name('logOut');
     //forgot
